8a2b56f50c4f6602f4fb16a8bf2e381b6e38600c
isaias <isaiaslozada19@gmail.com>
2025-08-20 09:22:00 -0400

    Agregando la seccion de bienvenida

--- src/modules/home/components/FeaturedProducts.jsx
+++ src/modules/home/components/FeaturedProducts.jsx
@@ -1,14 +1,15 @@
 import { Grid, Container } from "@mui/material";
-import { products } from "../utils/products"
-import { ProductCard } from "./ProductCard"
+import { products } from "../utils/DataproductsHomePage"
+import { ProductCard } from "./ProductCardHomePage"
+import { SectionTitle } from "./SectionTitle"
 
 export function FeaturedProducts() {
     return (
       <Container sx={{ py: 6 }}>
-        {/* <SectionTitle
+        <SectionTitle
           title="Productos Destacados"
           subtitle="Los productos más populares entre nutricionistas profesionales"
-        /> */}
+        />
         <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 16 }}>
           {products.map((product) => (
             <Grid item key={product.id} size={{ xs: 2, sm: 4, md: 4 }}
---
+++ src/modules/home/components/SectionTitle.jsx
@@ -0,0 +1,16 @@
+import { Typography, Box } from "@mui/material";
+
+export function SectionTitle({ title, subtitle }){
+    return (
+        <Box textAlign="center" mb={4}>
+          <Typography variant="h5" fontWeight="bold" gutterBottom>
+            {title}
+          </Typography>
+          {subtitle && (
+            <Typography variant="body2" color="text.secondary">
+              {subtitle}
+            </Typography>
+          )}
+        </Box>
+      );
+}
