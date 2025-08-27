51e6d690d83d8472ece6b4a6223785ceb8ccfa04
marioAmauta <mariodevcl@gmail.com>
2025-08-26 23:00:06 -0400

    mover imagenes a carpeta public y actualizar sus referencias en el codigo

--- src/modules/home/utils/DataSpecializedCategories.js
+++ src/modules/home/utils/DataSpecializedCategories.js
@@ -10,8 +10,8 @@ export const categories = [
     icon: StraightenIcon,
     count: 24,
     countText: "24 productos",
-    image: "src/assets/equipos medicion.jpg",
-    url: "/src/assets/categoria/equipos-de-medicion"
+    image: "/equipos-medicion.jpg",
+    url: "/categoria/equipos-de-medicion"
   },
   {
     name: "Comunidad Nutricional",
@@ -19,8 +19,8 @@ export const categories = [
     icon: GroupIcon,
     count: 5000,
     countText: "5000+ Miembros",
-    image: "src/assets/comunidad nutricion.png",
-    url: "/src/assets/categoria/cursos-isak"
+    image: "/comunidad-nutricion.png",
+    url: "/categoria/cursos-isak"
   },
   {
     name: "Recursos Digitales",
@@ -28,8 +28,8 @@ export const categories = [
     icon: CalculateIcon,
     count: 45,
     countText: "45 recursos",
-    image: "src/assets/recursos digitales.webp",
-    url: "src/assets/categoria/recursos-digitales"
+    image: "/recursos-digitales.webp",
+    url: "/categoria/recursos-digitales"
   },
   {
     name: "Box de Atención",
@@ -37,7 +37,7 @@ export const categories = [
     icon: LocationCityIcon,
     count: 32,
     countText: "32 boxes",
-    image: "src/assets/box nutricional.jpg",
-    url: "src/assets/categoria/box-de-atencion"
+    image: "/box-nutricional.jpg",
+    url: "/categoria/box-de-atencion"
   }
 ];
--- src/modules/home/utils/DataproductsHomePage.js
+++ src/modules/home/utils/DataproductsHomePage.js
@@ -1,47 +1,51 @@
-
 export const products = [
-    {
-      id: 1,
-      title: "Caliper Harpenden",
-      description: "Calibrador Harpenden para medición precisa de pliegues cutáneos",
-      category: "Equipos de medición",
-      price: 1290000,
-      stock: 1,
-      image: "src/assets/Plicometro HARPENDEN.jpg",
-      tag: "Más Vendido",
-    },
-    {
-      id: 2,
-      title: "Balanza Mecánica",
-      descripton: "aaa",
-      category: "Equipos de medición",
-      price: 599990,
-      stock: 2,
-      image: "src/assets/Balanza mecanica.png",
-      tag: "Nuevo",
-    },
-    {
-      id: 3,
-      title: "Balanza Pediátrica",
-      description: "eeee",
-      category: "Equipos de medición",
-      price: 399990,
-      stock: 20,
-      image: "src/assets/Balanza pediatrica.jpg",
-      tag: "Oferta",
-    },
-    {
-      id: 4,
-      title: "Cinta métrica Lufkin",
-      description: "oooo",
-      category: "Plantillas",
-      price: 27990,
-      stock: 40,
-      image: "src/assets/Cinta metrica Lufkin.webp",
-      tag: "Oferta",
-    },
-  ];
+  {
+    id: 1,
+    title: "Caliper Harpenden",
+    description: "Calibrador Harpenden para medición precisa de pliegues cutáneos",
+    category: "Equipos de medición",
+    price: 1290000,
+    stock: 1,
+    image: "/plicometro-harpen.jpg",
+    tag: "Más Vendido"
+  },
+  {
+    id: 2,
+    title: "Balanza Mecánica",
+    descripton: "aaa",
+    category: "Equipos de medición",
+    price: 599990,
+    stock: 2,
+    image: "/balanza-mecanica.png",
+    tag: "Nuevo"
+  },
+  {
+    id: 3,
+    title: "Balanza Pediátrica",
+    description: "eeee",
+    category: "Equipos de medición",
+    price: 399990,
+    stock: 20,
+    image: "/balanza-pediatrica.jpg",
+    tag: "Oferta"
+  },
+  {
+    id: 4,
+    title: "Cinta métrica Lufkin",
+    description: "oooo",
+    category: "Plantillas",
+    price: 27990,
+    stock: 40,
+    image: "/cinta-metrica-lufkin.webp",
+    tag: "Oferta"
+  }
+];
 
-export const categories = ["Todos", "Equipos de medición", "Cursos", "Plantillas", "Libros", "Software"];
-
-  
+export const categories = [
+  "Todos",
+  "Equipos de medición",
+  "Cursos",
+  "Plantillas",
+  "Libros",
+  "Software"
+];
